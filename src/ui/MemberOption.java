39e9efdc9d43ff03a5efb748d608159d3430b807
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-05-08 12:35:06 +0200

    Added JavaDoc to MemberOption.java

--- src/ui/MemberOption.java
+++ src/ui/MemberOption.java
@@ -1,5 +1,8 @@
 package ui;
 
+/**
+ * Represents the selectable options in the Member menu, each with a numeric value and a display label.
+ */
 public enum MemberOption {
 
     OPTION_1(1, "View portfolio"),
@@ -11,19 +14,36 @@ public enum MemberOption {
     private final int value;
     private final String label;
 
+    /**
+     * @param value the numeric choice the user types to select this option
+     * @param label the description of the value that will be printed
+     */
     MemberOption(int value, String label) {
         this.value = value;
         this.label = label;
     }
 
+    /**
+     * Getter for the value attribute.
+     * @return the numeric identifier shown in the menu
+     */
     public int getValue() {
         return value;
     }
-
+    /**
+     * Getter for the label attribute.
+     * @return the display text shown next to the menu number
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * Returns the MemberOption whose value matches the given integer.
+     * @param value the integer the user entered
+     * @return the MemberOption matching the given value
+     * @throws IllegalArgumentException if no option matches the given value
+     */
     public static MemberOption fromChoice(int value) {
         for (MemberOption option : values()) {
             if (option.value == value) return option;
