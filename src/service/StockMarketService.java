d3f6ab374f8ff12615eb1d79f7b6d515a30cb1ed
nickichandk <nicklas.marc@gmail.com>
2026-05-08 13:12:39 +0200

    Updated the stock and asset class with fields and getters - "removed some", so everything gets displayed with information and not "null" Updated the stock market display

--- src/service/StockMarketService.java
+++ src/service/StockMarketService.java
@@ -101,9 +101,9 @@ public class StockMarketService {
      * </p>
      */
     public void viewMarket(){
-        System.out.println(Colors.MENUHEADER + "\n===== STOCK MARKET =====" + Colors.RESET);
-        System.out.printf("%-8s %-30s %-15s %10s %6s%n", "TICKER", "NAME", "SECTOR", "PRICE", "CURR");
-        System.out.println("-".repeat(73));
+        System.out.println(Colors.MENUHEADER + "\n============================== STOCK MARKET ===============================\n" + Colors.RESET);
+        System.out.printf("%-10s %-30s %-14s %11s %6s%n", "TICKER", "NAME", "SECTOR", "PRICE", "CURR");
+        System.out.println("-".repeat(75));
 
         for(Stock stock : stockMap.values()){
             System.out.println(Colors.MENUOPTION + stock + Colors.RESET);
